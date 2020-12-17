d0264e49f040b54c7feec12d32bed3f77221400a
weiyutang <weiyutang1010@outlook.com>
2020-12-17 20:35:41 +0800

    Done styling card

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
-import "./App.css";
+import { Container } from "react-bootstrap";
+import WebinarInfoCard from "./WebinarInfoCard";
 
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+const dummyInfo = {
+	topic: 'Computer Science > Database',
+	title: 'How to Design a Relational Database',
+	host: 'Arthur Hiew',
+	date: 'Sat 12/12/2020',
+	startTime: '2:00pm',
+	endTime: '3:00m'
+}
+
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World</h1>
-			{/* <Router>
-				<Switch></Switch>
-			</Router> */}
+			<Container className="d-flex justify-content-between flex-wrap w-75 mt-5">
+				<WebinarInfoCard info={dummyInfo}/>
+				<WebinarInfoCard info={dummyInfo}/>
+				<WebinarInfoCard info={dummyInfo}/>
+				<WebinarInfoCard info={dummyInfo}/>
+				<WebinarInfoCard info={dummyInfo}/>
+				<WebinarInfoCard info={dummyInfo}/>
+				<WebinarInfoCard info={dummyInfo}/>
+			</Container>
 		</div>
 	);
 }
