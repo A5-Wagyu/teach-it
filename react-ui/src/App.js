ed8929b8e5bf3fc41f95f66354ceccde74ec1144
Arthur Hiew <ahfx8518@hotmail.com>
2020-12-17 00:12:13 -0800

    initial commit (create-react-app)

---
+++ react-ui/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+function App() {
+	return (
+		<div className="App">
+			<h1>Hello World</h1>
+			{/* <Router>
+				<Switch></Switch>
+			</Router> */}
+		</div>
+	);
+}
+
+export default App;
---
+++ server/server.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const path = require("path");
+const cluster = require("cluster");
+const numCPUs = require("os").cpus().length;
+const cors = require("cors");
+const isDev = process.env.NODE_ENV !== "production";
+const PORT = process.env.PORT || 5000;
+const bodyParser = require("body-parser");
+const MysqlPoolBooster = require("mysql-pool-booster");
+var mysql = require("mysql");
+
+require("dotenv").config({ path: "./../.env" });
+
+mysql = MysqlPoolBooster(mysql);
+const config = {
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	connectionLimit: 10,
+	maxIdle: 1,
+	multipleStatements: true,
+};
+
+const pool = mysql.createPool(config);
+
+console.log("Testing Connection");
+pool.query("SELECT 1 + 1 AS solution", function (error, results, fields) {
+	if (error) throw Error("Could not connect to DB!");
+	if (results[0].solution == 2) {
+		console.log("Connection GOOD!");
+	} else {
+		console.log("Connection BAD!");
+	}
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Multi-process to utilize all CPU cores.
+if (!isDev && cluster.isMaster) {
+	console.error(`Node cluster master ${process.pid} is running`);
+
+	// Fork workers.
+	for (let i = 0; i < numCPUs; i++) {
+		cluster.fork();
+	}
+
+	cluster.on("exit", (worker, code, signal) => {
+		console.error(
+			`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`
+		);
+	});
+} else {
+	// Priority serve any static files.
+	app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
+
+	// Answer API requests.
+	app.get("/api", function (req, res) {
+		res.set("Content-Type", "application/json");
+		res.send('{"message":"Hello from the custom server!"}');
+	});
+
+	// All remaining requests return the React app, so it can handle routing.
+	app.get("*", function (request, response) {
+		response.sendFile(
+			path.resolve(__dirname, "../react-ui/build", "index.html")
+		);
+	});
+
+	app.listen(PORT, function () {
+		console.error(
+			`Node ${
+				isDev ? "dev server" : "cluster worker " + process.pid
+			}: listening on port ${PORT}`
+		);
+	});
+}
