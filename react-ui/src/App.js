5bef5ab1e0cc806eba15ec199d4c39497608f940
Arthur Hiew <ahfx8518@hotmail.com>
2020-12-17 12:39:48 -0800

    moved WebinarInfoCard to components

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,30 +1,7 @@
 import React from "react";
-import { Container } from "react-bootstrap";
-import WebinarInfoCard from "./pages/WebinarInfoCard";
-
-const dummyInfo = {
-	topic: "Computer Science > Database",
-	title: "How to Design a Relational Database",
-	host: "Arthur Hiew",
-	date: "Sat 12/12/2020",
-	startTime: "2:00pm",
-	endTime: "3:00m",
-};
 
 function App() {
-	return (
-		<div className="App">
-			<Container className="d-flex justify-content-between flex-wrap w-75 mt-5">
-				<WebinarInfoCard info={dummyInfo} />
-				<WebinarInfoCard info={dummyInfo} />
-				<WebinarInfoCard info={dummyInfo} />
-				<WebinarInfoCard info={dummyInfo} />
-				<WebinarInfoCard info={dummyInfo} />
-				<WebinarInfoCard info={dummyInfo} />
-				<WebinarInfoCard info={dummyInfo} />
-			</Container>
-		</div>
-	);
+	return <div className="App"></div>;
 }
 
 export default App;
