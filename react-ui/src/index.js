07fcb7c33135436df8b2e92b5ce4f3fcea003be2
Arthur Hiew <ahfx8518@hotmail.com>
2020-12-17 20:23:19 -0800

    fix missing unique keys in drop down navbar dropdown

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -2,12 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter } from "react-router-dom";
-ReactDOM.render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
-	</React.StrictMode>,
-	document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
