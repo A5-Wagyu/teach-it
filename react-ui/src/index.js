eb18c260ca2155e50e285c07a5c41bce837e7281
Arthur Hiew <ahfx8518@hotmail.com>
2020-12-18 14:49:41 -0800

    unable to get topic id

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -2,4 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-ReactDOM.render(<App />, document.getElementById("root"));
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.render(
+	<BrowserRouter>
+		<App />
+	</BrowserRouter>,
+	document.getElementById("root")
+);
