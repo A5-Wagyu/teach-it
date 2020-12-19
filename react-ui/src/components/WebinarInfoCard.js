da936e67bdb2fc3b90eb1a989515b1d3d6b2bb73
Arthur Hiew <ahfx8518@hotmail.com>
2020-12-18 22:45:09 -0800

    get subtopic from db

--- react-ui/src/components/WebinarInfoCard.js
+++ react-ui/src/components/WebinarInfoCard.js
@@ -2,35 +2,36 @@ import React from "react";
 import { Card, Container } from "react-bootstrap";
 
 export const WebinarInfoCard = ({
-  name,
-  topic,
-  title,
-  host,
-  date,
-  startTime,
-  endTime,
-  onClick,
+	name,
+	topic,
+	subtopic,
+	title,
+	host,
+	date,
+	startTime,
+	endTime,
+	onClick,
 }) => {
-  return (
-    <Card
-      className="rounded mr-3 mb-3 text-justify"
-      style={{ width: "20rem" }}
-      onClick={onClick}
-    >
-      <Card.Header>{topic}</Card.Header>
-      <Card.Body>
-        <Card.Title name={name} className="mb-4" style={{ height: "35%" }}>
-          {title}
-        </Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{host}</Card.Subtitle>
-        <hr />
-        <div className="mb-1">
-          <span>{date}</span>
-          <span className="float-right">
-            {startTime} - {endTime} PT
-          </span>
-        </div>
-      </Card.Body>
-    </Card>
-  );
+	return (
+		<Card
+			className="rounded mr-3 mb-3 text-justify"
+			style={{ width: "20rem" }}
+			onClick={onClick}
+		>
+			<Card.Header>{topic}</Card.Header>
+			<Card.Body>
+				<Card.Title name={name} className="mb-4" style={{ height: "35%" }}>
+					{title}
+				</Card.Title>
+				<Card.Subtitle className="mb-2 text-muted">{host}</Card.Subtitle>
+				<hr />
+				<div className="mb-1">
+					<span>{date}</span>
+					<span className="float-right">
+						{startTime} - {endTime} PT
+					</span>
+				</div>
+			</Card.Body>
+		</Card>
+	);
 };
