eaabf7377ac863cabc5626004fc0fcfac39d4a62
weiyutang <weiyutang1010@outlook.com>
2020-12-19 12:35:10 +0800

    fix webinar card height

--- react-ui/src/components/WebinarInfoCard.js
+++ react-ui/src/components/WebinarInfoCard.js
@@ -1,26 +1,26 @@
 import React from "react";
-import { Card } from "react-bootstrap";
+import { Card, Container } from "react-bootstrap";
 
 export const WebinarInfoCard = ({
-	topic,
-	title,
-	host,
-	date,
-	startTime,
-	endTime,
+  topic,
+  title,
+  host,
+  date,
+  startTime,
+  endTime,
 }) => {
-	return (
-		<Card className="rounded mr-3 mb-3 text-justify" style={{ width: "20rem" }}>
-			<Card.Header>{topic}</Card.Header>
-			<Card.Body>
-				<Card.Title className="mb-4">{title}</Card.Title>
-				<Card.Subtitle className="mb-2 text-muted">{host}</Card.Subtitle>
-				<hr />
-				<span>{date}</span>
-				<span className="float-right">
-					{startTime} - {endTime} PT
-				</span>
-			</Card.Body>
-		</Card>
-	);
+  return (
+    <Card className="rounded mr-3 mb-3 text-justify" style={{ width: "20rem" }}>
+      <Card.Header>{topic}</Card.Header>
+      <Card.Body>
+        <Card.Title className="mb-4 h-25">{title}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted">{host}</Card.Subtitle>
+        <hr />
+        <span>{date}</span>
+        <span className="float-right">
+          {startTime} - {endTime} PT
+        </span>
+      </Card.Body>
+    </Card>
+  );
 };
