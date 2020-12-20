b70448a09f126cba4e910476df05257332ed42b8
QuanDev2 <qnguyen.dev2@gmail.com>
2020-12-20 01:50:52 -0800

    set global state in app.js for authentication

--- react-ui/src/components/Header.js
+++ react-ui/src/components/Header.js
@@ -7,27 +7,25 @@ import { DropdownSubmenu, NavDropdownMenu } from "react-bootstrap-submenu";
 import { useAuth } from "../contexts/authContext";
 
 
-function Header(props) {
+function Header({ isAuthenticated, setIsAuthenticated, history }) {
 	const [topics, setTopics] = useState([]);
 	const [subtopics, setSubtopics] = useState([]);
 	const [values, setValues] = useState({});
-	const [isAuthenticated, setIsAuthenticated] = useState(false);
 	const { verifyLocalToken, logout } = useAuth();
 
 	const res = verifyLocalToken();
 
 
-  const getTopicsQuery = () => {
-    getTopics().then(function (t) {
-      setTopics(t.data);
-    });
-  };
+	const getTopicsQuery = () => {
+		getTopics().then(function (t) {
+			setTopics(t.data);
+		});
+	};
 
 
 	const handleLogout = async () => {
 		await logout();
 		setIsAuthenticated(false);
-		console.log(props);
 		// props.history.push("/");
 		return (
 			<Redirect to="/" />
@@ -43,118 +41,118 @@ function Header(props) {
 	};
 
 
-  const getSubtopicsQuery = () => {
-    getSubtopics().then(function (st) {
-      setSubtopics(st.data);
-      countTopic(st.data);
-    });
-  };
+	const getSubtopicsQuery = () => {
+		getSubtopics().then(function (st) {
+			setSubtopics(st.data);
+			countTopic(st.data);
+		});
+	};
 
-  useEffect(() => {
-    getTopicsQuery();
-  }, []);
+	useEffect(() => {
+		getTopicsQuery();
+	}, []);
 
-  useEffect(() => {
-    getSubtopicsQuery();
-  }, []);
+	useEffect(() => {
+		getSubtopicsQuery();
+	}, []);
 
 
-  return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand as={Link} to="/">
-        Teach It
+	return (
+		<Navbar bg="light" expand="lg">
+			<Navbar.Brand as={Link} to="/">
+				Teach It
       </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr">
-          <NavDropdownMenu
-            title="Browse Topics"
-            id="collasible-nav-dropdown"
-            className="mr-3"
-          >
-            {topics.map((topic, i, array) => {
-              if (topic.id in values) {
-                return (
-                  <DropdownSubmenu key={topic.name + i} title={topic.name}>
-                    {subtopics.map((subtopic, j, arrayJ) => {
-                      if (topic.id === subtopic.topicID) {
-                        return (
-                          <NavDropdown.Item
-                            as="div"
-                            key={subtopic.name + i + j}
-                          >
-                            <Link
-                              className="text-reset"
-                              to={{
-                                pathname: "/search",
-                                state: {
-                                  subtopic: subtopic,
-                                },
-                              }}
-                            >
-                              {subtopic.name}
-                            </Link>
-                          </NavDropdown.Item>
-                        );
-                      } else {
-                        return null;
-                      }
-                    })}
-                  </DropdownSubmenu>
-                );
-              } else {
-                return (
-                  <NavDropdown.Item as="div" key={i} title={topic.name}>
-                    <Link
-                      className="text-reset"
-                      to={{ pathname: "/search", state: { topic: topic } }}
-                    >
-                      {topic.name}
-                    </Link>
-                  </NavDropdown.Item>
-                );
-              }
-            })}
-          </NavDropdownMenu>
-        </Nav>
-      </Navbar.Collapse>
-      {isLoggedIn && (
-        <div className="loggedIn d-flex align-items-center">
-          <p className="mb-0"> Welcome back, {currentUserName}</p>
-          <Nav.Item>
-            <Nav.Link href="/mylearning">My Learning</Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link href="/myteaching">My Teaching</Nav.Link>
-          </Nav.Item>
-          <Link to="/createwebinar">
-            <Button variant="outline-info" className="mr-3">
-              Create Webinar
+			<Navbar.Toggle aria-controls="basic-navbar-nav" />
+			<Navbar.Collapse id="basic-navbar-nav">
+				<Nav className="mr">
+					<NavDropdownMenu
+						title="Browse Topics"
+						id="collasible-nav-dropdown"
+						className="mr-3"
+					>
+						{topics.map((topic, i, array) => {
+							if (topic.id in values) {
+								return (
+									<DropdownSubmenu key={topic.name + i} title={topic.name}>
+										{subtopics.map((subtopic, j, arrayJ) => {
+											if (topic.id === subtopic.topicID) {
+												return (
+													<NavDropdown.Item
+														as="div"
+														key={subtopic.name + i + j}
+													>
+														<Link
+															className="text-reset"
+															to={{
+																pathname: "/search",
+																state: {
+																	subtopic: subtopic,
+																},
+															}}
+														>
+															{subtopic.name}
+														</Link>
+													</NavDropdown.Item>
+												);
+											} else {
+												return null;
+											}
+										})}
+									</DropdownSubmenu>
+								);
+							} else {
+								return (
+									<NavDropdown.Item as="div" key={i} title={topic.name}>
+										<Link
+											className="text-reset"
+											to={{ pathname: "/search", state: { topic: topic } }}
+										>
+											{topic.name}
+										</Link>
+									</NavDropdown.Item>
+								);
+							}
+						})}
+					</NavDropdownMenu>
+				</Nav>
+			</Navbar.Collapse>
+			{isAuthenticated && (
+				<div className="loggedIn d-flex align-items-center">
+					<Link to="/mylearning">
+						<Nav.Item>
+							<Nav.Link >My Learning</Nav.Link>
+						</Nav.Item>
+					</Link>
+
+					<Nav.Item>
+						<Nav.Link >My Teaching</Nav.Link>
+					</Nav.Item>
+					<Link to="/createwebinar">
+						<Button variant="outline-info" className="mr-3">
+							Create Webinar
             </Button>
-          </Link>
-          <Link to="/logout">
-            <Button variant="info" className="mr-3">
-              Log Out
+					</Link>
+					<Button variant="info" className="mr-3" onClick={handleLogout}>
+						Log Out
             </Button>
-          </Link>
-        </div>
-      )}
-      {!isLoggedIn && (
-        <div className="notLoggedIn">
-          <Link to="/login">
-            <Button variant="outline-info" className="mr-3">
-              Log In
+				</div>
+			)}
+			{!isAuthenticated && (
+				<div className="notLoggedIn">
+					<Link to="/login">
+						<Button variant="outline-info" className="mr-3">
+							Log In
             </Button>
-          </Link>
-          <Link to="/signup">
-            <Button variant="info" className="mr-3">
-              Sign Up
+					</Link>
+					<Link to="/signup">
+						<Button variant="info" className="mr-3">
+							Sign Up
             </Button>
-          </Link>
-        </div>
-      )}
-    </Navbar>
-  );
+					</Link>
+				</div>
+			)}
+		</Navbar>
+	);
 
 }
 
