af8f6265618614202d4044a223e23b9db9beecbb
jmetz6 <metzjo@oregonstate.edu>
2020-12-20 00:27:05 -0800

    Im GOing impl

--- react-ui/src/pages/Search.js
+++ react-ui/src/pages/Search.js
@@ -174,6 +174,7 @@ const Search = (props) => {
         handleClose={(e) => {
           setShow(false);
         }}
+        id={webinar.id}
         title={webinar.title}
         description={webinar.description}
         host={webinar.host}
